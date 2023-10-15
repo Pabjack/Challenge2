58c7a07ed1f38b8a60197bd1e5184cfc8fb93d01
MarioGuerrero19 <18032568@itcelaya.edu.mx>
2023-10-14 23:02:11 -0600

    Esto es una prueba de que llego la actualizacion

--- src/test/java/equipo7/challenge2/Challenge2ApplicationTests.java
+++ src/test/java/equipo7/challenge2/Challenge2ApplicationTests.java
@@ -2,7 +2,7 @@ package equipo7.challenge2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//Hola espero que este comentario se vea
 @SpringBootTest
 class Challenge2ApplicationTests {
 
