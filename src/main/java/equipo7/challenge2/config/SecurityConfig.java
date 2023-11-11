f25aff7fec7386ff1d6627a782aa16c972904421
pabloVD <pablo_daw@hotmail.com>
2023-11-11 14:02:19 -0600

    avance de

--- src/main/java/equipo7/challenge2/config/SecurityConfig.java
+++ src/main/java/equipo7/challenge2/config/SecurityConfig.java
@@ -22,5 +22,6 @@ public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Excepti
                     .authenticated()
             ).formLogin(withDefaults())
             .build();
+
 }
 }
