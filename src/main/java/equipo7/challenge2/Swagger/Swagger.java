94ec9c3fac1c1671d54b98041a522458f1d2ca11
Omar0 <omar060601@gmail.com>
2023-12-11 22:12:11 -0600

    sin errores en el swagger

--- src/main/java/equipo7/challenge2/Swagger/Swagger.java
+++ src/main/java/equipo7/challenge2/Swagger/Swagger.java
@@ -1,11 +1,12 @@
-package equipo7.challenge2.Swagger;
+package equipo7.challenge2.swagger;
 
-import com.google.common.base.Predicate;
+import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,12 +16,26 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class Swagger{
 
     @Bean
-    public Docket api() {
+    public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis((RequestHandlerSelectors.basePackage("equipo7.challenge2.controllers"))
+                .apis(RequestHandlerSelectors.basePackage("equipo7.challenge2.controllers"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                        .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo(){
+        return new ApiInfo(
+                "Order Service API",
+                "Order Service API Description",
+                "1.0",
+                "http://metaphorce.com",
+                new Contact("metaphorce", "https://metaphorce.com", "apis@metaphorce.com"),
+                "LICENSE",
+                "LICENSE URL",
+                Collections.emptyList()
+        );
     }
 }
 
